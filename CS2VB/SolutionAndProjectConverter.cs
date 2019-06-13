2d8354d111cfd5fe569139260d14cd77b50a9a07
Roman <romankalachik@gmail.com>
2019-06-13 18:32:40 +0300

    added files

---
+++ CS2VB/SolutionAndProjectConverter.cs
@@ -0,0 +1,21 @@
+﻿using CodeConverter.Tests.TestRunners;
+using ICSharpCode.CodeConverter.VB;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS2VB
+{
+    public class SolutionAndProjectConverter : ProjectConverterBase
+    {
+        public async Task ConvertSolution()
+        {
+            await ConvertProjectsWhere<CSToVBConversion>(p => true);
+        }
+
+        public async Task ConvertSingleProject()
+        {
+            await ConvertProjectsWhere<CSToVBConversion>(p => p.Name == "CSharpConsoleApp");
+        }
+    }
+}
